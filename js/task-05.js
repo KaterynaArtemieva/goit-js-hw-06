ffa25e69449cedfe8deea25de4a9e4123abb7c49
Kateryna Artemieva <moruga.katya@gmail.com>
2023-03-22 15:59:52 +0000

    task-5-done

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const refs = {
+  input: document.querySelector("#name-input"),
+  name: document.querySelector("#name-output"),
+};
+
+refs.input.addEventListener("input", (event) => {
+  refs.name.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    refs.name.textContent = "Anonymous";
+  }
+});
