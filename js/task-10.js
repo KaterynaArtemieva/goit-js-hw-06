95829b24053a3955921c9eef697a53dcdedc6503
Kateryna Artemieva <moruga.katya@gmail.com>
2023-03-23 16:05:52 +0000

    task-10-start

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
+const controlsEl = document.querySelector('#controls > input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+let inputValue = 0;
+
+controlsEl.addEventListener('blur', onControlsElBlur);
+createBtn.addEventListener('click', onCreateBtnClick);
+destroyBtn.addEventListener('click', onDestroyBtnClick);
+
+function onControlsElBlur () {
+  inputValue = controlsEl.value;
+}
+
+
+function onCreateBtnClick () {
+do {
+  const boxItem = document.createElement('div');
+  boxItem.style.width = 30 + 'px';
+  boxItem.style.height = 30 + 'px';
+  boxItem.style.backgroundColor = getRandomHexColor();
+} while (inputValue >= boxesEl)
+}
+
+
+function onDestroyBtnClick () {
+
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
