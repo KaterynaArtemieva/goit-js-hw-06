39d392eadbf2c110a40a0cea047631eb661c453f
Kateryna Artemieva <moruga.katya@gmail.com>
2023-03-24 13:59:39 +0000

    task-10-done

--- js/task-10.js
+++ js/task-10.js
@@ -1,33 +1,32 @@
-const controlsEl = document.querySelector('#controls > input');
-const createBtn = document.querySelector('button[data-create]');
-const destroyBtn = document.querySelector('button[data-destroy]');
-const boxesEl = document.querySelector('#boxes');
+const controlsEl = document.querySelector("#controls > input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
 
 let inputValue = 0;
 
-controlsEl.addEventListener('blur', onControlsElBlur);
-createBtn.addEventListener('click', onCreateBtnClick);
-destroyBtn.addEventListener('click', onDestroyBtnClick);
+controlsEl.addEventListener("blur", onControlsElBlur);
+createBtn.addEventListener("click", onCreateBtnClick);
+destroyBtn.addEventListener("click", onDestroyBtnClick);
 
-function onControlsElBlur () {
-  inputValue = controlsEl.value;
+function onControlsElBlur() {
+  inputValue = Number(controlsEl.value);
 }
 
-
-function onCreateBtnClick () {
-do {
-  const boxItem = document.createElement('div');
-  boxItem.style.width = 30 + 'px';
-  boxItem.style.height = 30 + 'px';
-  boxItem.style.backgroundColor = getRandomHexColor();
-} while (inputValue >= boxesEl)
+function onCreateBtnClick() {
+  for (let i = 1, j = 30; i <= inputValue; i += 1, j += 10) {
+    const boxItem = document.createElement("div");
+    boxItem.style.width = j + "px";
+    boxItem.style.height = j + "px";
+    boxItem.style.backgroundColor = getRandomHexColor();
+    boxesEl.append(boxItem);
+  }
 }
 
-
-function onDestroyBtnClick () {
-
+function onDestroyBtnClick() {
+  const clearBox = "";
+  boxesEl.innerHTML = clearBox;
 }
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
